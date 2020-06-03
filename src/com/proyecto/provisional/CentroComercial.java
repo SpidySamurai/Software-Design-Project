2beedf15d96ee13f72ca24f2dcf3fbfa62117154
SpidySamurai <javierchiortiz@gmail.com>
2020-06-03 01:40:38 -0500

    Clases e interfaces creadas, considerando atributos básicos y los patrones de diseño requeridos para el proyecto.

---
+++ src/com/proyecto/provisional/CentroComercial.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.proyecto.provisional;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Javier
+ */
+public class CentroComercial {
+    //Atributos básicos
+    private String nombreCentroComercial;
+    private ArrayList<Tienda> tiendas;
+    private ArrayList<Cliente> clientes; 
+    
+    public CentroComercial(){
+        
+    }
+    
+}
---
+++ src/com/proyecto/singleton/Singleton.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.proyecto.singleton;
+
+/**
+ *
+ * @author Javier
+ */
+public abstract class Singleton {
+    public abstract void getInstance();
+    
+}
