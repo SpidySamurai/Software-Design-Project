7e97efcae802eb9b47cb0ad9c9308b3eeacae873
llYOrchll <left4deadjgo@hotmail.com>
2020-06-04 16:37:13 -0500

    agregados los logos de las tiendas (Se reajusto tamaño de botones)

--- src/com/proyecto/vistas/EntradaCentro.java
+++ src/com/proyecto/vistas/EntradaCentro.java
@@ -31,9 +31,12 @@ public class EntradaCentro extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         btn_Carrito = new javax.swing.JButton();
+        tituloHeladeria = new javax.swing.JLabel();
+        tituloElectronica = new javax.swing.JLabel();
         btn_Heladeria = new javax.swing.JButton();
         btn_Electronica = new javax.swing.JButton();
-        brtn_Relojeria = new javax.swing.JButton();
+        tituloRelojeria = new javax.swing.JLabel();
+        btn_Relojeria = new javax.swing.JButton();
         msjBienvenida = new javax.swing.JLabel();
         Fondo = new javax.swing.JLabel();
 
@@ -41,10 +44,19 @@ public class EntradaCentro extends javax.swing.JFrame {
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        btn_Carrito.setText("Carrito");
-        jPanel1.add(btn_Carrito, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 0, 80, 30));
+        btn_Carrito.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/proyecto/imagenes/carro-de-la-compra (1).png"))); // NOI18N
+        jPanel1.add(btn_Carrito, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 0, 60, 50));
 
-        btn_Heladeria.setText("Heladeria");
+        tituloHeladeria.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
+        tituloHeladeria.setForeground(new java.awt.Color(255, 255, 255));
+        tituloHeladeria.setText("HELADERIA");
+        jPanel1.add(tituloHeladeria, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 110, -1, -1));
+
+        tituloElectronica.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
+        tituloElectronica.setText("ELECTRONICA");
+        jPanel1.add(tituloElectronica, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 260, -1, -1));
+
+        btn_Heladeria.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/proyecto/imagenes/HeladeriaLOGO.png"))); // NOI18N
         btn_Heladeria.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 btn_HeladeriaMouseEntered(evt);
@@ -53,13 +65,37 @@ public class EntradaCentro extends javax.swing.JFrame {
                 btn_HeladeriaMouseExited(evt);
             }
         });
-        jPanel1.add(btn_Heladeria, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 40, 200, 380));
+        btn_Heladeria.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_HeladeriaActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btn_Heladeria, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 60, 200, 380));
 
-        btn_Electronica.setText("Electronica");
-        jPanel1.add(btn_Electronica, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 40, 210, 380));
+        btn_Electronica.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/proyecto/imagenes/ElectronicaLOGO.png"))); // NOI18N
+        btn_Electronica.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_ElectronicaActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btn_Electronica, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 250, 430, 190));
 
-        brtn_Relojeria.setText("Relojeria");
-        jPanel1.add(brtn_Relojeria, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 40, 210, 380));
+        tituloRelojeria.setBackground(new java.awt.Color(0, 0, 0));
+        tituloRelojeria.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
+        tituloRelojeria.setForeground(new java.awt.Color(204, 204, 255));
+        tituloRelojeria.setText("RELOJERIA");
+        jPanel1.add(tituloRelojeria, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 70, 150, 20));
+
+        btn_Relojeria.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/proyecto/imagenes/RelojeriaLOGO (1).png"))); // NOI18N
+        btn_Relojeria.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btn_RelojeriaMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btn_RelojeriaMouseExited(evt);
+            }
+        });
+        jPanel1.add(btn_Relojeria, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 60, 430, 180));
 
         msjBienvenida.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         msjBienvenida.setForeground(new java.awt.Color(255, 255, 255));
@@ -67,7 +103,7 @@ public class EntradaCentro extends javax.swing.JFrame {
         jPanel1.add(msjBienvenida, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 0, 380, 30));
 
         Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/proyecto/imagenes/FondoInicio.jpg"))); // NOI18N
-        jPanel1.add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 660, 430));
+        jPanel1.add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 680, 450));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -84,12 +120,28 @@ public class EntradaCentro extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btn_HeladeriaMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_HeladeriaMouseEntered
-//        btn_Heladeria.setBackground(Color.red);
+        
     }//GEN-LAST:event_btn_HeladeriaMouseEntered
 
     private void btn_HeladeriaMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_HeladeriaMouseExited
-//        btn_Heladeria.setBackground(Color.blue);
+       
     }//GEN-LAST:event_btn_HeladeriaMouseExited
+
+    private void btn_RelojeriaMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_RelojeriaMouseEntered
+//        btn_Relojeria.setOpaque(false);
+    }//GEN-LAST:event_btn_RelojeriaMouseEntered
+
+    private void btn_RelojeriaMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_RelojeriaMouseExited
+//        btn_Relojeria.setOpaque(true);
+    }//GEN-LAST:event_btn_RelojeriaMouseExited
+
+    private void btn_HeladeriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_HeladeriaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn_HeladeriaActionPerformed
+
+    private void btn_ElectronicaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_ElectronicaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn_ElectronicaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -122,17 +174,21 @@ public class EntradaCentro extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new EntradaCentro().setVisible(true);
+                
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Fondo;
-    private javax.swing.JButton brtn_Relojeria;
     private javax.swing.JButton btn_Carrito;
     private javax.swing.JButton btn_Electronica;
     private javax.swing.JButton btn_Heladeria;
+    private javax.swing.JButton btn_Relojeria;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel msjBienvenida;
+    private javax.swing.JLabel tituloElectronica;
+    private javax.swing.JLabel tituloHeladeria;
+    private javax.swing.JLabel tituloRelojeria;
     // End of variables declaration//GEN-END:variables
 }
